7c4ae8c0a612f998224543462ed4517459118ae4
Skeleto <tardyfox@gmail.com>
2021-08-15 20:22:00 -0300

    Hello World added, with one quirk

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def hello
+        render html: "Hello, world! Time to praise our goddes hatsune miku i guess... i dont know sorry."
+    end
 end
