da0b11f3956e29d0a69cbd1bb05be9c52e57f710
Skeleto <tardyfox@gmail.com>
2021-08-26 16:04:08 -0300

    Functional user form with exception handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
 
+  def create 
+    @user = User.new(user_params)
+    if @user.save 
+        redirect_to @user
+    else
+      render 'new'
+    end
+  end 
+  
+  
   def show 
     @user = User.find(params[:id])
     
@@ -7,11 +17,13 @@ class UsersController < ApplicationController
   
   
   def new
+    @user = User.new
   end
 
   private   
     def user_params
-      params.require(:user).permit(:name, :email, :id)
+      params.require(:user).permit(:name, :email, :password, 
+                                   :password_confirmation)
     end 
   
 
